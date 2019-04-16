944c278995b6473957534afec792dc2feccd767f
chenxianjing <1531517063@qq.com>
2019-04-16 17:06:11 +0800

    加lombok

--- clienttest/src/main/java/org/cc/client/controller/ClientController.java
+++ clienttest/src/main/java/org/cc/client/controller/ClientController.java
@@ -4,13 +4,16 @@ import org.cc.client.server.ServerTestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
 @RestController
+@Slf4j
 public class ClientController {
 	@Autowired
 	private ServerTestService serverTestService;
 	@PostMapping("/client/testClient")
 	public String testClient() {
-		System.out.println(serverTestService.testServer());
+		log.info(serverTestService.testServer());
 		return "调用成功";
 	}
 }
