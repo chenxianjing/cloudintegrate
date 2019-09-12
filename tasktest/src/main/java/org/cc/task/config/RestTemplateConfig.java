09766be45318bcd6ff0864fb53726f7f20a7d24c
chhenxianjing <1531517063@qq.com>
2019-09-12 16:42:55 +0800

    init

--- tasktest/src/main/java/org/cc/task/config/RestTemplateConfig.java
+++ tasktest/src/main/java/org/cc/task/config/RestTemplateConfig.java
@@ -1,6 +1,16 @@
 package org.cc.task.config;
 
-import lombok.extern.slf4j.Slf4j;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.security.KeyManagementException;
+import java.security.KeyStoreException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.X509Certificate;
+import java.util.List;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+
 import org.apache.http.client.HttpClient;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
@@ -22,80 +32,72 @@ import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.ResponseErrorHandler;
 import org.springframework.web.client.RestTemplate;
 
-import javax.net.ssl.HostnameVerifier;
-import javax.net.ssl.SSLContext;
-import java.io.IOException;
-import java.nio.charset.Charset;
-import java.security.KeyManagementException;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.X509Certificate;
-import java.util.List;
+import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Configuration
 public class RestTemplateConfig {
 
-    @Bean
-    public RestTemplate restTemplate() {
-        RestTemplate restTemplate = new RestTemplate();
-        restTemplate.setRequestFactory(clientHttpRequestFactory());
-        restTemplate.setErrorHandler(new ResponseErrorHandler(){
+	@Bean
+	public RestTemplate restTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setRequestFactory(clientHttpRequestFactory());
+		restTemplate.setErrorHandler(new ResponseErrorHandler() {
 
-            @Override
-            public boolean hasError(ClientHttpResponse response) throws IOException {
-                return false;
-            }
+			@Override
+			public boolean hasError(ClientHttpResponse response) throws IOException {
+				return false;
+			}
 
-            @Override
-            public void handleError(ClientHttpResponse response) throws IOException {
+			@Override
+			public void handleError(ClientHttpResponse response) throws IOException {
 
-            }
-        });
-        //设置string消息转换器编码
-       /* List<HttpMessageConverter<?>> list = restTemplate.getMessageConverters();
-        for (HttpMessageConverter<?> httpMessageConverter : list) {
-            if(httpMessageConverter instanceof StringHttpMessageConverter) {
-                ((StringHttpMessageConverter) httpMessageConverter).setDefaultCharset(Charset.forName("utf-8"));
-                break;
-            }
-        }*/
-        return restTemplate;
-    }
+			}
+		});
+		// 设置string消息转换器编码
+		List<HttpMessageConverter<?>> list = restTemplate.getMessageConverters();
+		for (HttpMessageConverter<?> httpMessageConverter : list) {
+			if (httpMessageConverter instanceof StringHttpMessageConverter) {
+				((StringHttpMessageConverter) httpMessageConverter).setDefaultCharset(Charset.forName("utf-8"));
+				break;
+			}
+		}
+		return restTemplate;
+	}
 
-    @Bean
-    public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
-        try {
-            HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
-            SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
-                public boolean isTrusted(X509Certificate[] arg0, String arg1){
-                    return true;
-                }
-            }).build();
-            httpClientBuilder.setSSLContext(sslContext);
-            HostnameVerifier hostnameVerifier = NoopHostnameVerifier.INSTANCE;
-            SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContext,
-                    hostnameVerifier);
-            Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
-                    .register("http", PlainConnectionSocketFactory.getSocketFactory())
-                    .register("https", sslConnectionSocketFactory).build();// 注册http和https请求
-            // 开始设置连接池
-            PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager(
-                    socketFactoryRegistry);
-            poolingHttpClientConnectionManager.setMaxTotal(2700); // 最大连接数2700
-            poolingHttpClientConnectionManager.setDefaultMaxPerRoute(100); // 同路由并发数100
-            httpClientBuilder.setConnectionManager(poolingHttpClientConnectionManager);
-            httpClientBuilder.setRetryHandler(new DefaultHttpRequestRetryHandler(3, true)); // 重试次数
-            HttpClient httpClient = httpClientBuilder.build();
-            HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(
-                    httpClient); // httpClient连接配置
-            clientHttpRequestFactory.setConnectTimeout(20000); // 连接超时
-            clientHttpRequestFactory.setReadTimeout(30000); // 数据读取超时时间
-            clientHttpRequestFactory.setConnectionRequestTimeout(20000); // 连接不够用的等待时间
-            return clientHttpRequestFactory;
-        } catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
-            log.error("初始化HTTP连接池出错", e);
-        }
-        return null;
-    }
+	@Bean
+	public HttpComponentsClientHttpRequestFactory clientHttpRequestFactory() {
+		try {
+			HttpClientBuilder httpClientBuilder = HttpClientBuilder.create();
+			SSLContext sslContext = new SSLContextBuilder().loadTrustMaterial(null, new TrustStrategy() {
+				public boolean isTrusted(X509Certificate[] arg0, String arg1) {
+					return true;
+				}
+			}).build();
+			httpClientBuilder.setSSLContext(sslContext);
+			HostnameVerifier hostnameVerifier = NoopHostnameVerifier.INSTANCE;
+			SSLConnectionSocketFactory sslConnectionSocketFactory = new SSLConnectionSocketFactory(sslContext,
+					hostnameVerifier);
+			Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
+					.register("http", PlainConnectionSocketFactory.getSocketFactory())
+					.register("https", sslConnectionSocketFactory).build();// 注册http和https请求
+			// 开始设置连接池
+			PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager(
+					socketFactoryRegistry);
+			poolingHttpClientConnectionManager.setMaxTotal(2700); // 最大连接数2700
+			poolingHttpClientConnectionManager.setDefaultMaxPerRoute(100); // 同路由并发数100
+			httpClientBuilder.setConnectionManager(poolingHttpClientConnectionManager);
+			httpClientBuilder.setRetryHandler(new DefaultHttpRequestRetryHandler(3, true)); // 重试次数
+			HttpClient httpClient = httpClientBuilder.build();
+			HttpComponentsClientHttpRequestFactory clientHttpRequestFactory = new HttpComponentsClientHttpRequestFactory(
+					httpClient); // httpClient连接配置
+			clientHttpRequestFactory.setConnectTimeout(20000); // 连接超时
+			clientHttpRequestFactory.setReadTimeout(30000); // 数据读取超时时间
+			clientHttpRequestFactory.setConnectionRequestTimeout(20000); // 连接不够用的等待时间
+			return clientHttpRequestFactory;
+		} catch (KeyManagementException | NoSuchAlgorithmException | KeyStoreException e) {
+			log.error("初始化HTTP连接池出错", e);
+		}
+		return null;
+	}
 }
